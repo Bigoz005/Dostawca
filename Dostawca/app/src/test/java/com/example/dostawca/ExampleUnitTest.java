02e0c4504b401f251539a6ef54b33b9a3f18f5ae
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-11-06 02:13:51 +0100

    Unit test i layouty

--- Dostawca/app/src/test/java/com/example/dostawca/ExampleUnitTest.java
+++ Dostawca/app/src/test/java/com/example/dostawca/ExampleUnitTest.java
@@ -1,5 +1,16 @@
 package com.example.dostawca;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
