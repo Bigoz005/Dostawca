e1c8edf70b276d598c7f74bf9bb4cd03c522cc0e
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-11-25 17:54:26 +0100

    Google Location

--- Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
+++ Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
@@ -1,219 +1,260 @@
 package com.example.dostawca;
 
-import android.content.Context;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapFragment;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.maps.SupportMapFragment;
-import com.mapbox.mapboxsdk.utils.MapFragmentUtils;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MapFragments extends Fragment implements OnMapReadyCallback {
+public class MapFragments extends Fragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener, OnMapReadyCallback {
 
-    private final List<OnMapReadyCallback> mapReadyCallbackList = new ArrayList<>();
-    private MapFragment.OnMapViewReadyCallback mapViewReadyCallback;
-    private MapboxMap mapboxMap;
-    private MapView mapView;
+    private GoogleMap mMap;
 
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
-    /**
-     * Creates a default MapFragment instance
-     *
-     * @return MapFragment created
-     */
-    public static SupportMapFragment newInstance() {
-        return new SupportMapFragment();
+    private Location locationData;
+
+    private static final long TIME_INTERVAL_GET_LOCATION = 1000 * 5; // 1 Minute
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 5000;
+
+    public MapFragments() {
+        // Required empty public constructor
     }
-
-    /**
-     * Creates a MapFragment instance
-     *
-     * @param mapboxMapOptions The configuration options to be used.
-     * @return MapFragment created.
-     */
-    @NonNull
-    public static SupportMapFragment newInstance(@Nullable MapboxMapOptions mapboxMapOptions) {
-        SupportMapFragment mapFragment = new SupportMapFragment();
-        mapFragment.setArguments(MapFragmentUtils.createFragmentArgs(mapboxMapOptions));
-        return mapFragment;
-    }
-
-    /**
-     * Called when the context attaches to this fragment.
-     *
-     * @param context the context attaching
-     */
+    @Nullable
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof MapFragment.OnMapViewReadyCallback) {
-            mapViewReadyCallback = (MapFragment.OnMapViewReadyCallback) context;
-        }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, null);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+        TedPermission.with(getActivity())
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        return rootView;
     }
 
-    /**
-     * Called when this fragment is inflated, parses XML tag attributes.
-     *
-     * @param context            The context inflating this fragment.
-     * @param attrs              The XML tag attributes.
-     * @param savedInstanceState The saved instance state for the mapView fragment.
-     */
-    @Override
-    public void onInflate(@NonNull Context context, AttributeSet attrs, Bundle savedInstanceState) {
-        super.onInflate(context, attrs, savedInstanceState);
-        setArguments(MapFragmentUtils.createFragmentArgs(MapboxMapOptions.createFromAttributes(context, attrs)));
-    }
 
-    /**
-     * Creates the fragment view hierarchy.
-     *
-     * @param inflater           Inflater used to inflate content.
-     * @param container          The parent layout for the mapView fragment.
-     * @param savedInstanceState The saved instance state for the mapView fragment.
-     * @return The view created
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        ((MainActivity) getActivity()).setActionBarTitle("Map");
-        Mapbox.getInstance(getContext().getApplicationContext(), "pk.eyJ1IjoicHJvamVrdG1vYmxpbG5lIiwiYSI6ImNrMmt1ZmJ2dDAwYW0zZG96aHM3dzhkankifQ.5fek7SrkfE62NEHKUzHlrA");
-        Context context = inflater.getContext();
-        mapView = new MapView(context, MapFragmentUtils.resolveArgs(context, getArguments()));
-        return mapView;
-    }
-
-    /**
-     * Called when the fragment view hierarchy is created.
-     *
-     * @param view               The content view of the fragment
-     * @param savedInstanceState THe saved instance state of the framgnt
-     */
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-
-        // notify listeners about MapView creation
-        if (mapViewReadyCallback != null) {
-            mapViewReadyCallback.onMapViewReady(mapView);
-        }
-    }
-
-    @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
-        this.mapboxMap = mapboxMap;
-       mapboxMap.setStyle(new Style.Builder().fromUrl(Style.MAPBOX_STREETS));
-
-        for (OnMapReadyCallback onMapReadyCallback : mapReadyCallbackList) {
-            onMapReadyCallback.onMapReady(mapboxMap);
-        }
-    }
-
-    /**
-     * Called when the fragment is visible for the users.
-     */
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    /**
-     * Called when the fragment is ready to be interacted with.
-     */
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
     }
 
-    /**
-     * Called when the fragment is pausing.
-     */
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
     }
 
-    /**
-     * Called when the fragment state needs to be saved.
-     *
-     * @param outState The saved state
-     */
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (mapView != null) {
-            mapView.onSaveInstanceState(outState);
-        }
-    }
-
-    /**
-     * Called when the fragment is no longer visible for the user.
-     */
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    /**
-     * Called when the fragment receives onLowMemory call from the hosting Activity.
-     */
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        if (mapView != null) {
-            mapView.onLowMemory();
-        }
-    }
-
-    /**
-     * Called when the fragment is view hierarchy is being destroyed.
-     */
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mapView.onDestroy();
-    }
-
-    /**
-     * Called when the fragment is destroyed.
-     */
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mapReadyCallbackList.clear();
     }
 
-    /**
-     * Sets a callback object which will be triggered when the MapboxMap instance is ready to be used.
-     *
-     * @param onMapReadyCallback The callback to be invoked.
-     */
-    public void getMapAsync(@NonNull final OnMapReadyCallback onMapReadyCallback) {
-        if (mapboxMap == null) {
-            mapReadyCallbackList.add(onMapReadyCallback);
-        } else {
-            onMapReadyCallback.onMapReady(mapboxMap);
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+
+            // Create the LocationRequest object
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(TIME_INTERVAL_GET_LOCATION)    // 3 seconds, in milliseconds
+                    .setFastestInterval(TIME_INTERVAL_GET_LOCATION); // 1 second, in milliseconds
+
+
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.connect();
+                locationChecker(mGoogleApiClient, getActivity());
+            }
         }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+        }
+    };
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+
+        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                locationData = locationResult.getLastLocation();
+                // Toast.makeText(getActivity(), "Latitude: " + locationData.getLatitude() + ", Longitude: " + locationData.getLongitude(), Toast.LENGTH_SHORT).show();
+                //  Log.w("==>UpdateLocation<==", "" + String.format("%.6f", locationData.getLatitude()) + "," + String.format("%.6f", locationData.getLongitude()));
+
+                if (locationData != null) {
+
+                    LatLng point = new LatLng(locationData.getLatitude(), locationData.getLongitude());
+
+                    mMap.clear();
+
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(point).title("Your Current Location"));
+                    marker.showInfoWindow();
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder().target(point).zoom(16).build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                }
+
+            }
+        }, null);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        if (connectionResult.hasResolution() && getActivity() instanceof Activity) {
+            try {
+                Activity activity = (Activity) getActivity();
+                connectionResult.startResolutionForResult(activity, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i("", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+
+    }
+
+    public void locationChecker(GoogleApiClient mGoogleApiClient, final Activity activity) {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(activity, 1000);
+
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+
+
+        });
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //Log.w("==>UpdateLocation<==", "" + String.format("%.6f", location.getLatitude()) + "," + String.format("%.6f", location.getLongitude()));
+        //locationData = location;
+        //Toast.makeText(getActivity(), "Latitude: " + locationData.getLatitude() + ", Longitude: " + locationData.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(true);
+        //mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setIndoorEnabled(false);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+
+
+
     }
 }
