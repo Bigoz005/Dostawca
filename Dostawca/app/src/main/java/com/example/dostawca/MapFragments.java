6d5e2c57f407b095772725b4987bb000ebd62bef
Fine33e <damian.dluski@gmail.com>
2019-11-26 21:20:29 +0100

    QRscaner done

--- Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
+++ Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
@@ -143,7 +143,8 @@ public class MapFragments extends Fragment implements GoogleApiClient.Connection
     public void onConnected(@Nullable Bundle bundle) {
 
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
 
             return;
         }
