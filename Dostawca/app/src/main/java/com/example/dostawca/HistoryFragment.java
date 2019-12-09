97cced7d8ab558e07c6a8fe47140105cadb9d495
Michal Bartnicki <michalbartnicki7@gmail.com>
2019-12-09 23:46:56 +0100

    history view - display list of routes from firebase

--- Dostawca/app/src/main/java/com/example/dostawca/HistoryFragment.java
+++ Dostawca/app/src/main/java/com/example/dostawca/HistoryFragment.java
@@ -3,6 +3,7 @@ package com.example.dostawca;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +11,28 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.dostawca.dao.FirebaseDAO;
+import com.example.dostawca.dto.Point;
+import com.example.dostawca.dto.Route;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 
 public class HistoryFragment extends Fragment {
 
-    private ListView list;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> arrayList;
+    ArrayAdapter<String> listViewAdapter;
+    ArrayList<String> pointsHistory = new ArrayList<>();
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -30,14 +43,31 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ((MainActivity) getActivity()).setActionBarTitle("Historia");
-        ArrayList<String> pointsHistory = new ArrayList<>();
-        pointsHistory.add("Trasa testowa 1");
-        pointsHistory.add("Trasa testowa 2");
-        pointsHistory.add("Trasa testowa 3");
+
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.getReference().child("users").child("Du9Rue5ssZS4gYPmeUuL2rrKu4O2").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                Log.e("Count ", "" + snapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    Route route = postSnapshot.getValue(Route.class);
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy 'godz. 'hh:mm");
+                    String result = formatter.format(route.getCreated());
+                    pointsHistory.add(route.getName() + "\n" + "Utworzono: " + result);
+                    listViewAdapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Count ", "" + databaseError.getMessage());
+            }
+        });
 
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
-
-
         ListView lv = (ListView) rootView.findViewById(R.id.list);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -49,15 +79,16 @@ public class HistoryFragment extends Fragment {
                 startActivity(intent);
 
 
-
             }
         });
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+
+        listViewAdapter = new ArrayAdapter<String>(
                 getActivity(), android.R.layout.simple_list_item_1, pointsHistory
         );
 
         lv.setAdapter(listViewAdapter);
+
 
         return rootView;
 
--- Dostawca/app/src/main/java/com/example/dostawca/dao/FirebaseDAO.java
+++ Dostawca/app/src/main/java/com/example/dostawca/dao/FirebaseDAO.java
@@ -1,10 +1,24 @@
 package com.example.dostawca.dao;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.dostawca.dto.Route;
+import com.example.dostawca.dto.RouteList;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirebaseDAO {
     private FirebaseDatabase firebaseDatabase;
+    private List<Route> routes = new ArrayList<>();
 
 
     public FirebaseDAO() {
@@ -14,7 +28,31 @@ public class FirebaseDAO {
 
     public void saveRouteForCurrentUser(Route route) {
         //todo: get current user ID from service
-        firebaseDatabase.getReference().child("users").child("Du9Rue5ssZS4gYPmeUuL2rrKu4O2").setValue(route);
+        DatabaseReference pushedKey = firebaseDatabase.getReference().child("users").child("Du9Rue5ssZS4gYPmeUuL2rrKu4O2").push();
+        pushedKey.setValue(route);
+    }
+
+
+    public void getRoutes() {
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.getReference().child("users").child("Du9Rue5ssZS4gYPmeUuL2rrKu4O2").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.e("Count ", "" + snapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    Route route = postSnapshot.getValue(Route.class);
+                    routes.add(route);
+                    System.out.println("Got route: " + route.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Count ", "" + databaseError.getMessage());
+
+            }
+
+        });
     }
 
 
