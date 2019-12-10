1480968045e13ed97207ba2a0bda8132788ffdb2
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-12-10 14:37:04 +0100

    Merge of qrscanner

--- Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
+++ Dostawca/app/src/main/java/com/example/dostawca/MapFragments.java
@@ -143,6 +143,7 @@ public class MapFragments extends Fragment implements GoogleApiClient.Connection
     public void onConnected(@Nullable Bundle bundle) {
 
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             return;
