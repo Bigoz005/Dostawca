4d5c52c69ab2caa7fdccbb94e7d90d5d6ef258fb
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-12-18 04:01:04 +0100

    Qr scanner update working with ListofAddreses

--- Dostawca/app/src/main/java/com/example/dostawca/QRScannerFragment.java
+++ Dostawca/app/src/main/java/com/example/dostawca/QRScannerFragment.java
@@ -4,32 +4,51 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.dostawca.dto.Point;
+import com.example.dostawca.service.CurrentRouteService;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class QRScannerFragment extends Fragment {
+public class QRScannerFragment extends Fragment implements View.OnClickListener {
     private static final int CAMERA_REQUEST_CODE =100;
     SurfaceView surfaceView;
     CameraSource cameraSource;
     TextView textView;
     BarcodeDetector barcodeDetector;
+    Button buttonTakePhoto1;
+
     public QRScannerFragment() {
         // Required empty public constructor
     }
@@ -44,11 +63,12 @@ public class QRScannerFragment extends Fragment {
                 CAMERA_REQUEST_CODE,
                 Manifest.permission.CAMERA );
         ((MainActivity)getActivity()).setActionBarTitle("QR Scanner");
-
-        View view = inflater.inflate(R.layout.fragment_qrscanner, container, false);
+        View view = null;
+         view = inflater.inflate(R.layout.fragment_qrscanner, container, false);
 
         // Inflate the layout for this fragment
         surfaceView = (SurfaceView) view.findViewById(R.id.camerapreview);
+        buttonTakePhoto1 = (Button) view.findViewById(R.id.buttonTakePhoto1);
         textView = view.findViewById(R.id.QRText);
 
 
@@ -103,8 +123,82 @@ public class QRScannerFragment extends Fragment {
             }
         });
 
-
+        buttonTakePhoto1.setOnClickListener(this);
         return view;
     }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonTakePhoto1:
+                onAddressConfirm();
+                break;
+        }
+    }
+
+    public void onAddressConfirm() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        String address = textView.getText().toString();
+        String addressQuery = "";
+        try {
+            addressQuery = URLEncoder.encode(address, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            Toast.makeText(getActivity(), "Błąd w walidacji adresu", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+        if (addressQuery.length() < 3) {
+            Toast.makeText(getActivity(), "Podano za krótki adres", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // String key = "AIzaSyDhp7h_vV3XuKAOloMuG_fQMR9WE5yM12I";
+        // String url ="https://maps.googleapis.com/maps/api/geocode/json?address=" + addressQuery + "&key=" + key;
+        String url = "https://nominatim.openstreetmap.org/search?q=" + addressQuery + "&format=json";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        JSONArray places = null;
+                        try {
+                            places = new JSONArray(response);
+                        } catch (JSONException e) {
+                            Toast.makeText(getActivity(), "Błąd w walidacji adresu", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                        if (places.length() == 0) {
+                            Toast.makeText(getActivity(), "Taki adres nie istnieje!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            try {
+                                JSONObject result = places.getJSONObject(0);
+                                onAddressValidationSuccess(result.getString("lat"), result.getString("lon"));
+                            } catch (JSONException e) {
+                                Toast.makeText(getActivity(), "Błąd w walidacji adresu", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Błąd w walidacji adresu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(stringRequest);
+        Log.i("SCANNER", "Chosen address: " + address);
+    }
+
+    public void onAddressValidationSuccess(String lat, String lon) {
+        Toast.makeText(getActivity(), "Lat: " + lat + " Lon: " + lon, Toast.LENGTH_SHORT).show();
+        CurrentRouteService.addPointToCurrentRoute(new Point(textView.getText().toString(),
+                "",
+                lat, lon
+        ));
+
+    }
+
 
 }
